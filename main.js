76f719464808ba5024e8ee1fe0d9ccf5d2a9e785
Misty DeRamus <mistyderamus@gmail.com>
2019-06-06 16:47:07 -0500

    add get orders function

--- main.js
+++ main.js
@@ -4,22 +4,25 @@
 // Each meal object should have three properties: sandwichType, fries (true or false), and drinkSize.
 
 const restaurant = {
-  name: "Bob's Burgers",    //property
-  placeOrder: function(meal) {  //method
-    this.orders.push(meal)
+  name: "Bob's Burgers",                //property
+  placeOrder: function(meal) {     //method
+    this.orders.push(meal);
   },
-  orders: []                //array of object property
+  orders: [],                //array of object property
+  getOrder: function() {
+    return this.orders;
+  }
 };
 
 const chickenComboMeal = {
-    sandwichType: "Chicken Sandwich",
-    fries: true,
-    drinkSize: "large"
-
+  sandwichType: "Chicken Sandwich",
+  fries: true,
+  drinkSize: "large"
 };
 
 // Place an order
-restaurant.placeOrder();
+restaurant.placeOrder(chickenComboMeal);
 // Invoke the function to return the list of all orders
-
+restaurant.getOrder();
 // Output all orders to the console using console.table()
+console.table(restaurant.orders);
